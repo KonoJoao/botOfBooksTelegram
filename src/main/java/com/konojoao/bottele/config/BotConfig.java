6f856e1778e8b723794405bebe6ddeb58f3ae51a
João Vitor <76479617+KonoJoao@users.noreply.github.com>
2024-02-25 20:26:40 -0300

    Despersonalizando bot

--- src/main/java/com/konojoao/bottele/config/BotConfig.java
+++ src/main/java/com/konojoao/bottele/config/BotConfig.java
@@ -1,6 +1,6 @@
 package com.konojoao.bottele.config;
 
 public class BotConfig {
-    public static final String BOT_TOKEN = "6097559356:AAEXCbnBejPRRvx1GykNCu5ms4vsgRUUrmA";
-    public static final String BOT_NAME = "Denjus";
+    public static final String BOT_TOKEN = "";
+    public static final String BOT_NAME = "";
 }
